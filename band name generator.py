f33e38cddc3913a9979bf05423cfe68f190b3333
thaslimthoufica <126398997+thaslimthoufica@users.noreply.github.com>
2024-03-28 19:34:50 +0530

    Create band name generator.py

---
+++ band name generator.py
@@ -0,0 +1,4 @@
+print("welcome to the band generator")
+user_name = input("what is your town name?\n")
+pet_name = input("what is your pet name?\n")
+print("your band name could be"+"" + user_name +" " + pet_name)
