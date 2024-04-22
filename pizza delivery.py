0244c88306ce35b8821e597ae55bfe4b8a6d1efd
thaslimthoufica <126398997+thaslimthoufica@users.noreply.github.com>
2024-04-22 17:01:15 +0530

    description added
    Based on a user's order, work out their final bill.
    
    Small pizza (S): $15
    
    Medium pizza (M): $20
    
    Large pizza (L): $25
    
    Add pepperoni for small pizza (Y or N): +$2
    
    Add pepperoni for medium or large pizza (Y or N): +$3
    
    Add extra cheese for any size pizza (Y or N): +$1

--- pizza delivery.py
+++ pizza delivery.py
@@ -2,8 +2,6 @@ print("Thank you for choosing Python Pizza Deliveries!")
 size = input() # What size pizza do you want? S, M, or L
 add_pepperoni = input() # Do you want pepperoni? Y or N
 extra_cheese = input() # Do you want extra cheese? Y or N
-# 🚨 Don't change the code above 👆
-# Write your code below this line 👇
 if size=="S":
     bill=15
     if add_pepperoni=="Y":
